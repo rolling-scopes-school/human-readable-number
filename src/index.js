1f55726eaab4a7446bb1ac04396739728eba8a54
Musa98 <www.musik2014@mail.ru>
2020-10-18 15:39:31 +0300

    feat: chancge 1000 numbers into string words

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ module.exports = function toReadable (number) {
   let result = '';
   let stringNumber = '';
 
+  // Функция вывода текстового значения числа, в зависимости от разряда
   const checkDischarge = (number, mas) =>{
     let n;
 
@@ -30,13 +31,15 @@ module.exports = function toReadable (number) {
 
   numberLength = +numberLength.length;
 
+  // Проверка на 1 разряд
   if(numberLength === 1){
     return checkDischarge(number, firstDischargeMas);
-  } 
+  }
+  //Проверка на 2 разряда от 10 до 19 
   if(numberLength === 2 && number < 20){
     return checkDischarge(number, secondDischargeMas);
   }
-
+  // Проверка на 2 разряда от 20 до 99
   if(numberLength === 2 && number >= 20){
     stringNumber = '' + number;
     result += checkDischarge(+stringNumber[0], thirdDischargeMas);
@@ -45,6 +48,36 @@ module.exports = function toReadable (number) {
     }
     result += ' ' + checkDischarge(+stringNumber[1], firstDischargeMas);
     return result;
+  }
+  //Проверка на 3 разряда
+
+  if(numberLength === 3){
+    stringNumber = '' + number;
+    result += checkDischarge(+stringNumber[0], firstDischargeMas);
+    result +=' hundred';
+    // Числа от 100 до 109
+    if(+stringNumber[1] === 0){
+        if(+stringNumber[2] === 0){
+            return result; 
+         }
+        result +=' ' + checkDischarge(+stringNumber[2], firstDischargeMas);
+        return result;
+    }
+    // Числа от 110 до 119
+    if(+stringNumber[1] === 1){
+        stringNumber = stringNumber[1] + stringNumber[2];
+        result +=' ' + checkDischarge(+stringNumber, secondDischargeMas);
+        return result;
+    }
+    // Числа от 120 до 999
+    if(+stringNumber[1] >= 2){
+        result +=' ' + checkDischarge(+stringNumber[1], thirdDischargeMas);
+        if(+stringNumber[2] === 0){
+           return result; 
+        }
+        result += ' ' + checkDischarge(+stringNumber[2], firstDischargeMas);
+        return result;
+    }
   }
 
 
