0bda753a1b31256d72f4f83927674f236bea031b
Taneros <fricentaurus@GMAIL.COM>
2021-07-24 20:04:19 +0300

    feat: add my file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    // const numberToWords = require('number-to-words');
+    // console.log(numberToWords.toWords(number));
+    // return numberToWords.toWords(number);
+    return numberToEnglish(number);
 }
