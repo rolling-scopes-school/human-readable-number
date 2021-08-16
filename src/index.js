9da8d1029c6e7f5cc67ceb486450e11ae9dcb9a2
shamedzka <shamedzka@gmail.com>
2021-08-17 02:30:22 +0300

    1-99

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable(number) {
-   const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   const arr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
    const arrDozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
    let i = 0;
    let readableNumber = '';
@@ -7,9 +7,18 @@ module.exports = function toReadable(number) {
    if (number.toString().length === 3 && number % 100 === 0) {
       return `${arr[Math.floor(number / 100)]} hundred`;
    }
-   else if (number.toString().length === 1) {
+   else if (number.toString().length === 1 && number !== 0) {
       return `${arr[number % 10]}`;
-   } else {
-      return zero;
+   }
+   else if (number === 0) {
+      return 'zero';
+   }
+   else if (number.toString().length === 2 && number > 9 && number < 20) {
+      return `${arr[number % 100]}`;
+   }
+   else if (number.toString().length === 2 && number > 20) {
+      return `${arrDozens[Math.floor(number / 10)]} ${arr[number % 10]}`;
+   }
+   else {
    }
 }
