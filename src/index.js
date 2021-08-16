6e18ea6da457e3ef00a501fe3cac99718f374b42
shamedzka <shamedzka@gmail.com>
2021-08-17 01:58:23 +0300

    hundred

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+   const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   const arrDozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+   let i = 0;
+   let readableNumber = '';
+
+   if (number.toString().length === 3) {
+      return `${arr[Math.floor(number / 100)]} hundred`;
+   }
+   return zero;
 }
