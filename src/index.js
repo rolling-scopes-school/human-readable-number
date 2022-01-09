fb09eac65a083654451008f49f08d56b75f78bdb
Andrew <lazerabate9119@gmail.com>
2022-01-09 15:03:38 +0300

    fix: change error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ module.exports = function toReadable (num) {
     return( smallNum[num-1]);
   }else if(num > 10 && num <20){
     return( middleNum[num-10-1]);
-  } else if (num%10 == 0){
+  } else if (num%10 == 0 && num<100){
     return( bigNum[num/10-1]);
   }else if(num >20){
     let numb = String(num).split('');
@@ -24,14 +24,14 @@ module.exports = function toReadable (num) {
       }
       return (str);
       }else if(leng == 3 ){
-        if(numb[1] == 1){
+        if(numb[1] == 1 && numb[2] != 0){
           str += smallNum[numb[0] - 1] + ' hundred ' + middleNum[numb[2] - 1]        
         }else if(numb[1] == 0 && numb[2] == 0){
-          str += smallNum[numb[0] - 1] + ' hundred '  
-        }else if(numb[1] == 0){
-          str += smallNum[numb[0] - 1] + ' hundred ' + smallNum[numb[2] - 1] 
+          str += smallNum[numb[0] - 1] + ' hundred'  
         }else if(numb[2] == 0){
           str += smallNum[numb[0] - 1] + ' hundred ' + bigNum[numb[1] - 1] 
+        }else if(numb[1] == 0){
+          str += smallNum[numb[0] - 1] + ' hundred ' + smallNum[numb[2] - 1] 
         }else {
           str += smallNum[numb[0] - 1] + ' hundred ' +  bigNum[numb[1] - 1] + ' ' + smallNum[numb[2] - 1]
       }
