d29fc4b9c4d383b69494591c0f7e6bd35ea8fed2
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-07-12 13:22:01 +0300

    in process, feat: ones done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    str = number.toString();
+    let res;
+    if (str.length === 1) {
+      // console.log("yes");
+      let ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+      ];
+      ones.forEach((el) => {
+          // console.log(ones.indexOf(el) + ': ' + el);
+        if (number === ones.indexOf(el)) {
+          res = el;
+          return res;
+        }
+      });
+    }
+  //   return res;
+    console.log(res);
+  }
