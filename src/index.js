a00b2954b4d04c49763f23010751102d0d4e8f0d
Aleks-Korvin <aleks.porvin@gmail.com>
2022-07-29 00:45:24 +0300

    fix: js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 module.exports = function toReadable(number) {
-    if ((toReadable = toReadable.toString()).length > 9) return "overflow";
-    n = ("000000000" + number)
+    let a = [
+        "",
+        "one ",
+        "two ",
+        "three ",
+        "four ",
+        "five ",
+        "six ",
+        "seven ",
+        "eight ",
+        "nine ",
+        "ten ",
+        "eleven ",
+        "twelve ",
+        "thirteen ",
+        "fourteen ",
+        "fifteen ",
+        "sixteen ",
+        "seventeen ",
+        "eighteen ",
+        "nineteen ",
+    ];
+    let b = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if ((number = number.toString()).length > 9) return "overflow";
+    let n = ("000000000" + number)
         .substr(-9)
         .match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
     if (!n) return;
-    let str = "";
+    var str = "";
     str +=
         n[1] != 0
             ? (a[Number(n[1])] || b[n[1][0]] + " " + a[n[1][1]]) + "crore "
