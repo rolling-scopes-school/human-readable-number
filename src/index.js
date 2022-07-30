24aef0f22c3370bc38837330b0395fd47d601ba8
Aleks-Korvin <aleks.porvin@gmail.com>
2022-07-31 00:06:59 +0300

    fix: js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable(number) {
-    var a = [
+    const first = [
         "",
         "one ",
         "two ",
@@ -21,7 +21,7 @@ module.exports = function toReadable(number) {
         "eighteen ",
         "nineteen ",
     ];
-    var b = [
+    const tens = [
         "",
         "",
         "twenty",
@@ -33,34 +33,35 @@ module.exports = function toReadable(number) {
         "eighty",
         "ninety",
     ];
+    const mad = ["", "thousand", "million", "billion", "trillion"];
+    let word = "";
 
-    if ((number = number.toString()).length > 9) return "overflow";
-    n = ("000000000" + number)
-        .substr(-9)
-        .match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return;
-    var str = "";
-    str +=
-        n[1] != 0
-            ? (a[Number(n[1])] || b[n[1][0]] + " " + a[n[1][1]]) + "crore "
-            : "";
-    str +=
-        n[2] != 0
-            ? (a[Number(n[2])] || b[n[2][0]] + " " + a[n[2][1]]) + "lakh "
-            : "";
-    str +=
-        n[3] != 0
-            ? (a[Number(n[3])] || b[n[3][0]] + " " + a[n[3][1]]) + "thousand "
-            : "";
-    str +=
-        n[4] != 0
-            ? (a[Number(n[4])] || b[n[4][0]] + " " + a[n[4][1]]) + "hundred "
-            : "";
-    str +=
-        n[5] != 0
-            ? (str != "" ? "and " : "") +
-              (a[Number(n[5])] || b[n[5][0]] + " " + a[n[5][1]]) +
-              "only "
-            : "";
-    return str;
+    for (let i = 0; i < mad.length; i++) {
+        let tempNumber = number % (100 * Math.pow(1000, i));
+        if (Math.floor(tempNumber / Math.pow(1000, i)) !== 0) {
+            if (Math.floor(tempNumber / Math.pow(1000, i)) < 20) {
+                word =
+                    first[Math.floor(tempNumber / Math.pow(1000, i))] +
+                    mad[i] +
+                    " " +
+                    word;
+            } else {
+                word =
+                    tens[Math.floor(tempNumber / (10 * Math.pow(1000, i)))] +
+                    " " +
+                    first[Math.floor(tempNumber / Math.pow(1000, i)) % 10] +
+                    mad[i] +
+                    " " +
+                    word;
+            }
+        }
+
+        tempNumber = number % Math.pow(1000, i + 1);
+        if (Math.floor(tempNumber / (100 * Math.pow(1000, i))) !== 0)
+            word =
+                first[Math.floor(tempNumber / (100 * Math.pow(1000, i)))] +
+                "hunderd " +
+                word;
+    }
+    return word;
 };
