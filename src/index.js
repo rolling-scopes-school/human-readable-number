527911225a26c946fd04304645012430f7789be8
Aleks-Korvin <aleks.porvin@gmail.com>
2022-08-17 00:32:58 +0300

    feat: добавил новый файл с кодом

--- src/index.js
+++ src/index.js
@@ -1,67 +1,74 @@
+let th = ["", "thousand", "million", "billion", "trillion"];
+let dg = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+let tn = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+let tw = [
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
 module.exports = function toReadable(number) {
-    const first = [
-        "",
-        "one ",
-        "two ",
-        "three ",
-        "four ",
-        "five ",
-        "six ",
-        "seven ",
-        "eight ",
-        "nine ",
-        "ten ",
-        "eleven ",
-        "twelve ",
-        "thirteen ",
-        "fourteen ",
-        "fifteen ",
-        "sixteen ",
-        "seventeen ",
-        "eighteen ",
-        "nineteen ",
-    ];
-    const tens = [
-        "",
-        "",
-        "twenty",
-        "thirty",
-        "forty",
-        "fifty",
-        "sixty",
-        "seventy",
-        "eighty",
-        "ninety",
-    ];
-    const mad = ["", "thousand", "million", "billion", "trillion"];
-    let word = "";
-
-    for (let i = 0; i < mad.length; i++) {
-        let tempNumber = number % (100 * Math.pow(1000, i));
-        if (Math.floor(tempNumber / Math.pow(1000, i)) !== 0) {
-            if (Math.floor(tempNumber / Math.pow(1000, i)) < 20) {
-                word =
-                    first[Math.floor(tempNumber / Math.pow(1000, i))] +
-                    mad[i] +
-                    " " +
-                    word;
-            } else {
-                word =
-                    tens[Math.floor(tempNumber / (10 * Math.pow(1000, i)))] +
-                    " " +
-                    first[Math.floor(tempNumber / Math.pow(1000, i)) % 10] +
-                    mad[i] +
-                    " " +
-                    word;
+    number = number.toString();
+    number = number.replace(/[\, ]/g, "");
+    if (number != parseFloat(number)) return "not a number";
+    let x = number.indexOf(".");
+    if (x == -1) x = number.length;
+    if (x > 15) return "too big";
+    let n = number.split("");
+    let str = "";
+    let sk = 0;
+    for (let i = 0; i < x; i++) {
+        if ((x - i) % 3 == 2) {
+            if (n[i] == "1") {
+                str += tn[Number(n[i + 1])] + " ";
+                i++;
+                sk = 1;
+            } else if (n[i] != 0) {
+                str += tw[n[i] - 2] + " ";
+                sk = 1;
             }
+        } else if (n[i] != 0) {
+            // 0235
+            str += dg[n[i]] + " ";
+            if ((x - i) % 3 == 0) str += "hundred ";
+            sk = 1;
         }
-
-        tempNumber = number % Math.pow(1000, i + 1);
-        if (Math.floor(tempNumber / (100 * Math.pow(1000, i))) !== 0)
-            word =
-                first[Math.floor(tempNumber / (100 * Math.pow(1000, i)))] +
-                "hunderd " +
-                word;
+        if ((x - i) % 3 == 1) {
+            if (sk) str += th[(x - i - 1) / 3] + " ";
+            sk = 0;
+        }
     }
-    return word;
+
+    if (x != number.length) {
+        let y = number.length;
+        str += "point ";
+        for (let i = x + 1; i < y; i++) str += dg[n[i]] + " ";
+    }
+    return str.replace(/\s+/g, " ");
 };
