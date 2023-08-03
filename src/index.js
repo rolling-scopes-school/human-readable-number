4068a9a86cf00ebeb2c9f8b66acb5137b98d8d10
Musa98 <gsunnymus@gmail.com>
2023-08-04 01:46:30 +0300

    fix: optimize code

--- src/index.js
+++ src/index.js
@@ -1,85 +1,59 @@
 module.exports = function toReadable (number) {
-  const firstDischargeMas = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-  const secondDischargeMas = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-  const thirdDischargeMas = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-  let numberLength = number + '';
+  const firstDischargeArray = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  const secondDischargeArray = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const thirdDischargeArray = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  let numberLength = +(number + '').length;
   let result = '';
-  let stringNumber = '';
+  let stringNumber = number + '';
 
   // Функция вывода текстового значения числа, в зависимости от разряда
-  const checkDischarge = (number, mas) =>{
-    let n;
-
-    switch (mas){
-        case firstDischargeMas:
-            n = 0;
-            break;
-        case secondDischargeMas:
-            n = 10;
-            break;
-        case thirdDischargeMas:
-            n = 0;
-            break;
-    }
-
-    for(let i = 0; i < 10; i++){
-        if (number === i + n){
-            return mas[i];
-        }
-    }
+  const checkDischarge = (number, array) =>{
+    let n = (array === secondDischargeArray) ? 10 : 0;
+    return array.find((element, index) => number === index + n);
   }
-
-  numberLength = +numberLength.length;
 
   // Проверка на 1 разряд
   if(numberLength === 1){
-    return checkDischarge(number, firstDischargeMas);
+    return checkDischarge(number, firstDischargeArray);
   }
   //Проверка на 2 разряда от 10 до 19 
   if(numberLength === 2 && number < 20){
-    return checkDischarge(number, secondDischargeMas);
+    return checkDischarge(number, secondDischargeArray);
   }
   // Проверка на 2 разряда от 20 до 99
   if(numberLength === 2 && number >= 20){
-    stringNumber = '' + number;
-    result += checkDischarge(+stringNumber[0], thirdDischargeMas);
+    result += checkDischarge(+stringNumber[0], thirdDischargeArray);
     if(+stringNumber[1] === 0){
        return result; 
     }
-    result += ' ' + checkDischarge(+stringNumber[1], firstDischargeMas);
+    result += ` ${checkDischarge(+stringNumber[1], firstDischargeArray)}`;
     return result;
   }
   //Проверка на 3 разряда
-
   if(numberLength === 3){
-    stringNumber = '' + number;
-    result += checkDischarge(+stringNumber[0], firstDischargeMas);
-    result +=' hundred';
+    result += `${checkDischarge(+stringNumber[0], firstDischargeArray)} hundred`;
     // Числа от 100 до 109
     if(+stringNumber[1] === 0){
         if(+stringNumber[2] === 0){
             return result; 
          }
-        result +=' ' + checkDischarge(+stringNumber[2], firstDischargeMas);
+        result +=` ${checkDischarge(+stringNumber[2], firstDischargeArray)}`;
         return result;
     }
     // Числа от 110 до 119
     if(+stringNumber[1] === 1){
         stringNumber = stringNumber[1] + stringNumber[2];
-        result +=' ' + checkDischarge(+stringNumber, secondDischargeMas);
+        result +=` ${checkDischarge(+stringNumber, secondDischargeArray)}`;
         return result;
     }
     // Числа от 120 до 999
     if(+stringNumber[1] >= 2){
-        result +=' ' + checkDischarge(+stringNumber[1], thirdDischargeMas);
+        result +=` ${checkDischarge(+stringNumber[1], thirdDischargeArray)}`;
         if(+stringNumber[2] === 0){
            return result; 
         }
-        result += ' ' + checkDischarge(+stringNumber[2], firstDischargeMas);
+        result +=` ${checkDischarge(+stringNumber[2], firstDischargeArray)}`;
         return result;
     }
   }
-
-
-
 }
