95f5a367134fa73c93d0c778de3bffa8ae68d65e
Vlhome <kapustinvovik@gmail.com>
2024-10-16 21:34:15 +0300

    feat: add solution to human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const thirdNum = number % 10;
+    const secondNum = Math.floor((number % 100) / 10);
+    const firstNum = Math.floor(number / 100);
+    const result = [];
+
+    if (number === 0) {
+        return "zero";
+    }
+    if (firstNum > 0) {
+        result.push(
+            [
+                undefined,
+                "one hundred",
+                "two hundred",
+                "three hundred",
+                "four hundred",
+                "five hundred",
+                "six hundred",
+                "seven hundred",
+                "eight hundred",
+                "nine hundred",
+            ][firstNum]
+        );
+    }
+
+    if (secondNum === 1) {
+        result.push(
+            [
+                "ten",
+                "eleven",
+                "twelve",
+                "thirteen",
+                "fourteen",
+                "fifteen",
+                "sixteen",
+                "seventeen",
+                "eighteen",
+                "nineteen",
+            ][thirdNum]
+        );
+    } else {
+        if (secondNum > 1) {
+            result.push(
+                [
+                    undefined,
+                    undefined,
+                    "twenty",
+                    "thirty",
+                    "forty",
+                    "fifty",
+                    "sixty",
+                    "seventy",
+                    "eighty",
+                    "ninety",
+                ][secondNum]
+            );
+        }
+        if (thirdNum > 0) {
+            result.push(
+                [
+                    undefined,
+                    "one",
+                    "two",
+                    "three",
+                    "four",
+                    "five",
+                    "six",
+                    "seven",
+                    "eight",
+                    "nine",
+                ][thirdNum]
+            );
+        }
+    }
+
+    return result.join(" ");
+};
