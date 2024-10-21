4ac0cc6cdd88a6e8aa8f2d7875e2db98e89cc71d
Liliia <work_ft@protonmail.com>
2024-10-21 11:56:50 +0400

    solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', 'Ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numHundreds = Math.trunc(number / 100);
+    let balanceHundreds = number % 100;
+    let numTens = Math.trunc(balanceHundreds / 10);
+    let numUnits = balanceHundreds % 10;
+    let result = '';
+
+    if (number === 0) return result = 'zero';
+    if (number > 0 && number < 20) {
+        return result = units[number];
+    }
+
+    if (number >= 20 && number < 100 ) {
+        if (numUnits === 0) {
+           return result = tens[numTens];
+        } else return result = `${tens[numTens]} ${units[numUnits]}`;
+    }
+
+    if (number >= 100 && number < 1000) {
+        if (balanceHundreds === 0 && numUnits === 0) {
+            return result = `${units[numHundreds]} hundred`;
+        } else if (balanceHundreds < 20) {
+            return result = `${units[numHundreds]} hundred ${units[balanceHundreds]}`;
+        } else if (numUnits === 0) {
+            return result = `${units[numHundreds]} hundred ${tens[numTens]}`;
+        } else {
+            return result = `${units[numHundreds]} hundred ${tens[numTens]} ${units[numUnits]}`;
+        }
+    }
 }
